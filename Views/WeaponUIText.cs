97dda63f92132e56188079a65c6c0604d8b99d71
NEDNAR <mr.0vez@mail.ru>
2020-05-06 16:07:02 +0300

    FlashlightUIs and WeaponUIText added
    Всё ещё переписывал по скачанному коду

---
+++ Views/WeaponUIText.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public sealed class WeaponUIText : MonoBehaviour
+{
+    #region Fields
+
+    private Text _text;
+
+    #endregion
+
+
+    #region UnityMethods
+
+    private void Awake()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    public void ShowData(int countAmmunition, int countMag)
+    {
+        _text.text = $"{countAmmunition}/{countMag}";
+    }
+
+    public void SetActive(bool value)
+    {
+        _text.gameObject.SetActive(value);
+    }
+
+    #endregion
+}
