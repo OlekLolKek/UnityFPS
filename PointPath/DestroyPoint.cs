bc348430549483c4413736bb6743e707867770ed
NEDNAR <mr.0vez@mail.ru>
2020-05-12 19:07:56 +0300

    Placing path points added

---
+++ PointPath/DestroyPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+
+public class DestroyPoint : MonoBehaviour
+{
+    #region Fields
+
+    public event Action<GameObject> OnFinishChange = delegate (GameObject o) { };
+
+    #endregion
+
+
+    #region Methods
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Bot>())
+        {
+            OnFinishChange.Invoke(gameObject);
+        }
+    }
+
+    #endregion
+}
