57b89537319b9e000c4f22c0d61e7de72409513d
NEDNAR <mr.0vez@mail.ru>
2020-05-14 23:00:34 +0300

    Update CreateWayPointEditor.cs

--- Editor/CreateWayPointEditor.cs
+++ Editor/CreateWayPointEditor.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
+using UnityEditor.SceneManagement;
 
-public class CreateWayPointEditor : MonoBehaviour
+
+[CustomEditor(typeof(CreateWayPoint))]
+public class CreateWayPointEditor : UnityEditor.Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Fields
+
+    private CreateWayPoint _testTarget;
+
+    #endregion
+
+
+    #region UnityMethods
+
+    private void OnEnable()
     {
-        
+        _testTarget = (CreateWayPoint)target;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSceneGUI()
     {
-        
+        if (Event.current.button == 0 && Event.current.type == EventType.MouseDown)
+        {
+            Ray ray = Camera.current.ScreenPointToRay(new Vector3(Event.current.mousePosition.x,
+                SceneView.currentDrawingSceneView.camera.pixelHeight - Event.current.mousePosition.y));
+
+            if (Physics.Raycast(ray, out var hit))
+            {
+                _testTarget.InstantiateObj(hit.point);
+                SetObjectDirty(_testTarget.gameObject);
+            }
+        }
+        Selection.activeGameObject = _testTarget.gameObject;
     }
+
+    #endregion
+
+
+    #region Methods
+
+    public void SetObjectDirty(GameObject obj)
+    {
+        if (!Application.isPlaying)
+        {
+            EditorUtility.SetDirty(obj);
+            EditorSceneManager.MarkSceneDirty(obj.scene);
+        }
+    }
+
+    #endregion
 }
+#endif
+
