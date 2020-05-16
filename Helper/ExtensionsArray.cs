365b313346d93467c53c0de65bbde4002de3f40b
NEDNAR <mr.0vez@mail.ru>
2020-05-16 18:51:46 +0300

    Extension scripts

---
+++ Helper/ExtensionsArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+public static class ExtensionsArray
+{
+    #region Methods
+
+    public static T[] Concat<T>(this T[] x, T[] y)
+    {
+        if (x == null) throw new ArgumentNullException("x");
+        if (y == null) throw new ArgumentNullException("y");
+        var oldLen = x.Length;
+        Array.Resize(ref x, x.Length + y.Length);
+        Array.Copy(y, 0, x, oldLen, y.Length);
+        return x;
+    }
+
+    #endregion
+}
---
+++ Test/TestExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public sealed class TestExtension
+{
+    #region Methods
+
+    private void NameMethod()
+    {
+        List<int> list = new List<int>();
+        List<int> list2 = new List<int>();
+
+        list.Add(5);
+        5.AddList(list).AddList(list2);
+
+        bool test = "true".TryBool();
+
+        var myClass = new MyClass { A = 7 };
+        var c = myClass.DeepCopy();
+        c.A = 5;
+        Debug.Log(myClass.A);
+    }
+
+    #endregion
+
+    class MyClass
+    {
+        public int A;
+    }
+}
