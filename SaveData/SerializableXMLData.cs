caf821c108fd69be9f76910be8a109708274b283
NEDNAR <mr.0vez@mail.ru>
2020-05-18 20:35:09 +0300

    More Save & Load scripts

---
+++ SaveData/SerializableXMLData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+public class SerializableXMLData<T> : IData<T>
+{
+    #region Fields
+
+    private static XmlSerializer _formatter;
+
+    #endregion
+
+
+    #region ClassLifeCycle
+
+    public SerializableXMLData()
+    {
+        _formatter = new XmlSerializer(typeof(T));
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    public void Save(T data, string path = null)
+    {
+        using (var fs = new FileStream(path, FileMode.Create))
+        {
+            _formatter.Serialize(fs, data);
+        }
+    }
+
+    public T Load(string path)
+    {
+        T result;
+        if (!File.Exists(path)) return default(T);
+        using (var fs = new FileStream(path, FileMode.Open))
+        {
+            result = (T)_formatter.Deserialize(fs);
+        }
+        return result;
+    }
+
+    #endregion
+}
