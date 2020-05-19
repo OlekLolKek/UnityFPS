2ca887a5d45f989d55c365d06add6bea20bcc9bb
NEDNAR <mr.0vez@mail.ru>
2020-05-19 17:23:27 +0300

    Minimap & Radar

---
+++ Minimap/RadarObj.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RadarObj : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private Image _ico;
+
+    #endregion
+
+
+    #region UnityMethods
+
+    private void OnValidate()
+    {
+        _ico = Resources.Load<Image>("Image (1)");
+    }
+
+    private void OnDisable()
+    {
+        Radar.RemoveRadarObject(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        Radar.RegisterRadarObject(gameObject, _ico);
+    }
+
+    #endregion
+}
